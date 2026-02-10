443ffc005f07f3bc54be1f2153622646450b8dd3
julianaarruda <julianaarrud4@gmail.com>
2026-02-10 09:32:26 -0300

    atua

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,23 @@
-function Footer(){
-    return(
-        <>
-        </>
-    )
+//importando as imagens do footer
+
+import Logo from "../../assets/logo.png";
+import Whatsapp from "../../assets/whatsapp.png";
+import Instagram from "../../assets/instagram.png";
+import Linkedin from "../../assets/linkedin.png";
+
+//importando a folha de estilo do footer
+
+import styles from "./Footer.module.scss";
+
+function Footer() {
+  return (
+    <footer>
+      {/* Estrutura do footer  */}
+
+      <h2> Footer </h2>
+      
+    </footer>
+  );
 }
-export default Footer
+
+export default Footer;
